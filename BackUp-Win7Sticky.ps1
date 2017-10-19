6d6b98e388ce89fa760ce397a16bdf5e508d1c4d
Tom Dobson <dobson980@gmail.com>
2017-10-18 19:44:14 -0700

    Add files via upload

---
+++ BackUp-Win7Sticky.ps1
@@ -0,0 +1,108 @@
+﻿Function Backup-Win7Sticky {
+
+#region Backup-Win7Sticky Function Params
+Param(
+  [Parameter(Mandatory=$true, Position=0, HelpMessage="Please Provide Asset Tag `nExample: Backup-Win7Sticky -originatingPC IS1713922")]
+   [string]$originatingPC,
+	
+   [Parameter(Mandatory=$true, Position=1, HelpMessage="Please Provide Asset Tag `nExample: Backup-Win7Sticky -originatingPC IS1511410 -destinationPC IS1713922")]
+   [string]$destinationPC,
+
+   [Parameter(Mandatory=$true, Position=2, HelpMessage="Please Provide Asset Tag `nExample: Backup-Win7Sticky -originatingPC IS1511410 -destinationPC IS1713922 -forUser dobth")]
+   [string]$forUser
+)
+#endregion
+
+#region FilePath Variables
+$origPC = "\\$originatingPC\c$\Users\$forUser\AppData\Roaming\Microsoft\Sticky Notes\StickyNotes.snt"
+$destPC = "\\$destinationPC\c$\LDinst\StickyMigBackUp\ThresholdNotes.snt"
+#endregion
+
+#region Define Functions
+
+#Validate device is online and pinging within Sharp Network.
+Function Test-AssetOnline($pc) {
+    If ($pc) {
+	    If (Test-Connection -ComputerName $pc -Count 2 -BufferSize 16 -Quiet) {
+            write-host "Network Status:`n" -BackgroundColor Black -ForegroundColor white
+            write-host "$pc is Available `n" -BackgroundColor Green -ForegroundColor white
+            Write-Log -Message "$pc Is Available"
+            return $true | Out-Null
+            } else {
+            write-host "`nNetwork Status:" -BackgroundColor Black -ForegroundColor white
+            Write-Host "`n$pc Is Offline; Check Network Connection. `n" -BackgroundColor Red -ForegroundColor white
+            Write-Log -Level ERROR -Message "$pc OFFLINE"
+		    return $false | Out-Null
+	    }
+    }
+    Else {
+        return $false
+    }
+}
+
+#Migrates SNT file from Originating Win7 PC to Interim PC for temporary storage.
+Function Copy-SNTFile() {
+
+    If (Test-Path \\$originatingPC\c$\Users\$forUser) {
+        try {
+            New-Item \\$destinationPC\c$\LDinst\StickyMigBackUp -Type Directory -Force -ErrorAction Stop | Out-Null
+            $dirResult = "Created StickyMigBackUp Dir in LDlogs"
+        } catch {
+            $dirResult = "Failed to Created StickyMigBackUp Dir in LDlogs - $($_.exception.message)"         
+        }
+
+        try {
+            Copy-Item $origPC $destPC -Force -ErrorAction Stop
+            $result = "StickyNotes BackedUp Successfully to $destinationPC"
+        } catch {
+            $result = "Failed to back up Sticky Notes - $($_.exception.message)"
+        }
+
+        Write-Host "`n$dirResult`n`n$result`n"
+        Write-log -Message $dirResult
+        Write-Log -Message $result
+
+    } else {
+        Write-Host "No profile for $forUser exists on $originatingPC."
+        Write-Log "No profile for $forUser exists on $originatingPC. "
+    }
+
+}
+
+#endregion
+
+#region Logging Function ...
+Function Write-Log {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $False, HelpMessage = "Log Level")]
+		[ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+		[string]$Level = "INFO",
+		[Parameter(Mandatory = $True, Position = 0, HelpMessage = "Message to be written to the log")]
+		[string]$Message,
+		[Parameter(Mandatory = $False, HelpMessage = "Log file location and name")]
+		[string]$Logfile = "\\$destinationPC\c$\ldlogs\StickyMig.log"
+	)
+    BEGIN {
+    	$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+	    $Line = "$Stamp $Level $Message`r`n"
+    }
+    PROCESS {
+    	If ($Logfile) {
+            [System.IO.File]::AppendAllText($Logfile, $Line)
+	    } Else {
+		    Write-Output $Line
+	    }
+    }
+    END {}
+}
+#endregion
+
+#region MAIN
+Write-Log -Message "Backing Up Win7 Sticky Notes:"
+Test-AssetOnline($originatingPC)
+Test-AssetOnline($destinationPC)
+Copy-SNTFile
+#endregion
+
+}
---
+++ Migrate-ToWin10Sticky.ps1
@@ -0,0 +1,111 @@
+﻿Function Migrate-ToWin10Sticky {
+
+#region Migrate-ToWin10Sticky Function Params
+Param(
+  [Parameter(Mandatory=$true, Position=0, HelpMessage="Please Provide Asset Tag `nExample: Migrate-ToWin10Sticky -PCwithBackUpFile IS1713922")]
+   [string]$PCwithBackUpFile,
+	
+   [Parameter(Mandatory=$true, Position=1, HelpMessage="Please Provide Asset Tag `nExample: Migrate-ToWin10Sticky -PCwithBackUpFile IS1511410 -destinationWin10PC IS1713922")]
+   [string]$destinationWin10PC,
+
+   [Parameter(Mandatory=$true, Position=2, HelpMessage="Please Provide Asset Tag `nExample: Migrate-ToWin10Sticky -PCwithBackUpFile IS1511410 -destinationWin10PC IS1713922 -forUser dobth")]
+   [string]$forUser
+)
+#endregion
+
+#region FilePath Variables
+$origPC = "\\$PCwithBackUpFile\c$\ldinst\StickyMigBackUp\ThresholdNotes.snt"
+$destPC = "\\$destinationWin10PC\c$\Users\$forUser\AppData\Local\Packages\Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe\LocalState\Legacy\ThresholdNotes.snt"
+#endregion
+
+#region Define Functions
+
+#Validate device is online and pinging within Sharp Network.
+Function Test-AssetOnline($pc) {
+    If ($pc) {
+	    If (Test-Connection -ComputerName $pc -Count 2 -BufferSize 16 -Quiet) {
+            write-host "Network Status:`n" -BackgroundColor Black -ForegroundColor white
+            write-host "$pc is Available `n" -BackgroundColor Green -ForegroundColor white
+            Write-Log -Message "$pc Is Available"
+            return $true | Out-Null
+            } else {
+            write-host "`nNetwork Status:" -BackgroundColor Black -ForegroundColor white
+            Write-Host "`n$pc Is Offline; Check Network Connection. `n" -BackgroundColor Red -ForegroundColor white
+            Write-Log -Level ERROR -Message "$pc OFFLINE"
+		    return $false | Out-Null
+	    }
+    }
+    Else {
+        return $false
+    }
+}
+
+#Migrates SNT file from PC containing Win7 SNT Backup to Destination Win10 PC.
+Function Copy-SNTFile() {
+
+    If (Test-Path \\$destinationWin10PC\c$\Users\$forUser) {
+        try {
+            New-Item \\$destinationWin10PC\c$\Users\$forUser\AppData\Local\Packages\Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe\LocalState\Legacy -Type Directory -Force -ErrorAction Stop | Out-Null
+            $dirResult = "Created Legacy Dir in LocalState"
+        } catch {
+            $dirResult = "Failed to Create Legacy Dir in LocalState - $($_.exception.message)"         
+        }
+
+        try {
+            Copy-Item $origPC $destPC -Force -ErrorAction Stop
+            $result = "StickyNotes Migration Completed Successfully on $destinationWin10PC"
+            Remove-Item \\$PCwithBackUpFile\c$\ldinst\StickyMigBackUp -Force -Recurse
+        } catch {
+            $result = "Failed to Migrate Sticky Notes - $($_.exception.message)"
+        }
+
+        Write-Host "`n$dirResult`n`n$result`n"
+        Write-log -Message $dirResult
+        Write-Log -Message $result
+
+        
+
+    } else {
+        Write-Host "No profile for $forUser exists on $destinationWin10PC. User needs to log in before migration."
+        Write-Log "No profile for $forUser exists on $destinationWin10PC. User needs to log in before migration."
+    }
+
+}
+
+#endregion
+
+#region Logging Function ...
+Function Write-Log {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $False, HelpMessage = "Log Level")]
+		[ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+		[string]$Level = "INFO",
+		[Parameter(Mandatory = $True, Position = 0, HelpMessage = "Message to be written to the log")]
+		[string]$Message,
+		[Parameter(Mandatory = $False, HelpMessage = "Log file location and name")]
+		[string]$Logfile = "\\$PCwithBackUpFile\c$\ldlogs\StickyMig.log"
+	)
+    BEGIN {
+    	$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+	    $Line = "$Stamp $Level $Message`r`n"
+    }
+    PROCESS {
+    	If ($Logfile) {
+            [System.IO.File]::AppendAllText($Logfile, $Line)
+	    } Else {
+		    Write-Output $Line
+	    }
+    }
+    END {}
+}
+#endregion
+
+#region MAIN
+Write-Log -Message "Migrating Sticky Notes BackUp to Destination Win10 Device:"
+Test-AssetOnline($PCwithBackUpFile)
+Test-AssetOnline($destinationWin10PC)
+Copy-SNTFile
+#endregion
+
+}
